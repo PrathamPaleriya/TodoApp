4aef20bc69f24f5596bede37eee035b342e4ac36
PrathamPaleriyaQlink <pratham.paleriya@qlink.in>
2024-12-12 01:55:02 +0530

    fix

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -8,8 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      </head>
       <body className=''>
         <div className="w-full lg:px-20 px-5 py-3 flex justify-between items-center bg-[#7e64ff] text-white text-xl lg:text-2xl font-bold">
           TodoApp
