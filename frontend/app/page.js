da739bf452fd938cc95f28e0e51216135b4c8860
PrathamPaleriyaQlink <pratham.paleriya@qlink.in>
2024-12-12 02:01:54 +0530

    new frontend

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,148 +1,101 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import {
-  getTask,
-  addTask,
-  updateTask,
-  deleteTask,
-  ping,
-} from "../api/route";
-import MoonLoader from "react-spinners/ClipLoader";
-
+import Image from "next/image";
 
 export default function Home() {
-  const [taskValue, setTaskValue] = useState("");
-  const [tasks, setTasks] = useState([]);
-  const [loadingTaskId, setLoadingTaskId] = useState(null);
-  const [editingTaskId, setEditingTaskId] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false); 
-  const [modalTaskValue, setModalTaskValue] = useState("");
-  const [isDisable, setIsDisable] = useState(false);
- 
-  const handleSubmit = async () => {
-    setIsDisable(true);
-    if (taskValue) {  
-      await addTask(taskValue);
-      setTaskValue(""); 
-      fetchTask(); 
-    }
-    setIsDisable(false);
-  };
-
-  const handleMark = async (id, data) => {
-    setLoadingTaskId(id);
-    await updateTask(id, data);
-    fetchTask();
-  };
-
-  const handleEdit = (id, currentTask) => {
-    setEditingTaskId(id);
-    setModalTaskValue(currentTask); // Set the modal input field with the current task's value
-    setIsModalOpen(true); // Open the modal
-  };
-
-  const handleUpdate = async () => {
-    if (modalTaskValue && editingTaskId) {
-      await updateTask(editingTaskId, { "task": modalTaskValue });
-      setModalTaskValue("");
-      setEditingTaskId(null); 
-      setIsModalOpen(false); 
-      fetchTask();
-    }
-  };
-
-  const handleDelete = async (id) => {
-    setLoadingTaskId(id);
-    await deleteTask(id);
-    fetchTask();
-  }
-
-  const fetchTask = async () => {
-    const data = await getTask();
-    setTasks(data); 
-    setLoadingTaskId(null);
-  };
-
-  useEffect(() => {
-    ping();
-    fetchTask();
-  }, []);
-
-
   return (
-    <div className="lg:px-48 px-5">
-      <div className="flex items-center my-10 gap-8">
-        <input
-          type="text"
-          value={taskValue}
-          onChange={(e) => setTaskValue(e.target.value)}
-          placeholder="Enter your task"
-          className="flex-1 w-full border rounded-xl p-3 bg-gray-100"
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
         />
-        <button
-           onClick={handleSubmit}
-           disabled = {isDisable}
-          className={`w-fit h-full hover:bg-[#674ee1] px-10 py-3 rounded-xl text-white ${isDisable ? "bg-gray-500" : "bg-[#7e64ff]" }`}
-        >
-          Add Task
-        </button>
-      </div>
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              app/page.js
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-      <div className="text-3xl font-semibold">Your Tasks</div>
-      <div className="w-full my-5">
-        {tasks.length === 0 ?  (
-          <div className="text-gray-400"> No Task is there </div>
-        ) : (
-          tasks.map((task) => (
-            <div key={task._id} className="flex items-center w-full justify-between gap-5 px-5 py-2 border text-lg">
-              {loadingTaskId === task._id ? (
-                  <MoonLoader size={20} className="text-center" />
-                ) : (
-                  <input 
-                    type="checkbox" 
-                    checked={task.is_completed} 
-                    onChange={() => handleMark(task._id, { "is_completed": !task.is_completed })}
-                  />
-                )}
-              <div className={`w-full text-start ${task.is_completed ? 'text-gray-400 line-through' : 'text-black'}`}>{task.task}</div>
-              <div className="flex items-center justify-between gap-5">
-                <button className="text-blue-500 hover:underline hover:text-blue-700" onClick={() => handleEdit(task._id, task.task)}>Edit</button>
-                <button className="text-red-500 hover:underline hover:text-red-700" onClick={() => handleDelete(task._id)}>Delete</button>
-              </div>
-            </div>
-          ))
-        ) }
-      </div>
-
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-md flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-xl w-96">
-            <div className="text-xl font-semibold mb-4">Edit Task</div>
-            <input
-              type="text"
-              value={modalTaskValue}
-              onChange={(e) => setModalTaskValue(e.target.value)}
-              className="w-full border rounded-xl p-3 mb-4"
-              placeholder="Edit your task"
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
             />
-            <div className="flex justify-between px-5">
-              <button
-                onClick={() => setIsModalOpen(false)} // Close the modal
-                className="text-red-500"
-              >
-                Cancel
-              </button>
-              (<button
-                onClick={handleUpdate} // Update task and close modal
-                className="text-blue-500"
-              >
-                Update Task
-              </button>)
-            </div>
-          </div>
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      )}
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
